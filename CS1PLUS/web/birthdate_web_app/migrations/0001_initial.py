954d57d218b14a22c914fc3a687ad286c7acf7cd
SkiF4er <cajeerskif4er@gmail.com>
2026-03-05 20:12:15 +0300

    Add files via upload

--- CS1PLUS/web/birthdate_web_app/migrations/0001_initial.py
+++ CS1PLUS/web/birthdate_web_app/migrations/0001_initial.py
@@ -1,1 +1,22 @@
+from django.db import migrations, models
 
+class Migration(migrations.Migration):
+    initial = True
+
+    dependencies = []
+
+    operations = [
+        migrations.CreateModel(
+            name='HistoryEntry',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('user', models.CharField(default='anonymous', max_length=128)),
+                ('birth_date', models.DateField()),
+                ('weekday_ru', models.CharField(max_length=32)),
+                ('is_leap_year', models.BooleanField()),
+                ('age_years', models.IntegerField()),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+            ],
+            options={'ordering': ['-id']},
+        ),
+    ]
